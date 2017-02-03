e248748205b7036ffc9bffa345fbf3097b987d64
Luiz Pericolo <luiz.pericolo@b2wdigital.com>
2017-02-03 10:39:53 -0200

    update cron scripts

--- scripts/list_cron_tasks.py
+++ scripts/list_cron_tasks.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python
 
+import re
+
 from shell_cmds import run_command
 
-
 def get_cron_tasks():
-    return run_command("crontab -l").split("\n")
-
+    tasks = run_command('crontab -l')
+    if tasks:
+        return tasks.split('\n')
+    return []
 
 def list_cron_tasks():
     tasks = get_cron_tasks()
@@ -18,7 +21,7 @@ def print_cron_tasks(tasks):
         for i, task in enumerate(tasks):
             print("{} -> {}".format(i, task))
     else:
-        print("There are not cron tasks")
+        print("There are no cron tasks")
 
 if __name__ == "__main__":
     list_cron_tasks()
